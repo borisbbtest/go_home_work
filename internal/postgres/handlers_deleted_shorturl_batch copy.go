0c3204a272c43730e22bf78ae608b689f63e665b
Boris <Zelyanin>
2022-06-13 23:31:54 +0300

    d14

--- internal/postgres/handlers_deleted_shorturl_batch copy.go
+++ internal/postgres/handlers_deleted_shorturl_batch copy.go
@@ -18,7 +18,7 @@ func (p *Plugin) DeletedShortURLBatchURLHandler(conn *postgresConn, key string, 
 	ft := params[0].([]model.DataURL)
 	query := `UPDATE  public."storeurl"
 	          SET "StatusActive"= 2
-			  WHERE ShortPath = $1;`
+			  WHERE "ShortPath" = $1;`
 
 	for _, v := range ft {
 		_, err = conn.postgresPool.Exec(context.Background(), query, v.ShortPath)
