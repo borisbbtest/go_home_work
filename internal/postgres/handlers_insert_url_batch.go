491c490d31b717ef84f6d53b277c6a625d4fa809
Boris <Zelyanin>
2022-06-30 03:00:18 +0300

    d14

--- internal/postgres/handlers_insert_url_batch.go
+++ internal/postgres/handlers_insert_url_batch.go
@@ -2,8 +2,10 @@ package postgres
 
 import (
 	"context"
+	"time"
 
 	"github.com/borisbbtest/go_home_work/internal/model"
+	"github.com/jackc/pgx/v4"
 )
 
 const (
@@ -15,15 +17,22 @@ func (p *Plugin) insertBatchURLHandler(conn *postgresConn, key string, params []
 	var err error
 
 	ft := params[0].([]model.DataURL)
-	query := `INSERT INTO public."storeurl"(
-		"Port", "URL", "Path", "ShortPath", "UserID", "CorrelationId","StatusActive")
-		VALUES ($1, $2, $3, $4, $5, $6, 1);`
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancelFunc()
+
+	b := &pgx.Batch{}
 
 	for _, v := range ft {
-		_, err = conn.postgresPool.Exec(context.Background(), query, v.Port, v.URL, v.Path, v.ShortPath, v.UserID, v.CorrelationID)
-		if err != nil {
-			log.Error(err)
-		}
+		query := `INSERT INTO public."storeurl"(
+			"Port", "URL", "Path", "ShortPath", "UserID", "CorrelationId","StatusActive")
+			VALUES ($1, $2, $3, $4, $5, $6, 1);`
+		b.Queue(query, v.Port, v.URL, v.Path, v.ShortPath, v.UserID, v.CorrelationID)
+	}
+	conn.postgresPool.SendBatch(ctx, b)
+
+	if err != nil {
+		log.Error(err)
 	}
 
 	return nil, nil
